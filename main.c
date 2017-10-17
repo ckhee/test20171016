0f0576e2d85ab579c26640d3dcc1577c93459395
ckhee <ckhee@daful.net>
2017-10-17 13:06:50 +0900

    fixed #2 main.c 수정

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+int add(int a, int b)
+{
+	return a + b;
+}
+int sub(int a, int b)
+{
+	return a-b;
+}
+
+int div(int a, b)
+{
+	return a/b;
+}
+int multi(int a, int b)
+{
+	return a*b;
+}
 int main()
 {
+	int a,b,c;
+	return c;
 }
